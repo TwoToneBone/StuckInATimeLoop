941bc558c97f544935b468ebd22bb7c4dd89e659
Erik <erik.ra@gmail.com>
2017-04-23 14:14:21 +0200

    Add project files.

---
+++ StuckInATimeLoop/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StuckInATimeLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int spells = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < spells; i++)
+            {
+                Console.WriteLine($"{i +1 } Abracadabra");
+            }
+        }
+    }
+}
